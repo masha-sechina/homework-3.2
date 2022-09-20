1d147b8018e5308bab86046bb105402edbbd9fbe
Masha Sechina <mashka_3@mail.ru>
2022-09-20 14:27:18 +0300

    change2 homework-3.2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public int calculate (double a,int b){
+    public int calculate (double height,int weight){
         int bmi;
-        bmi = (int) (b / (a * a));
+        bmi = (int) (weight / (height * height));
 
         return bmi;
     }
